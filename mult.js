272aa1807ded5cf510f68a9d9e2cbc1bb9fd03a5
Anton Drelin <antondrelin@gmail.com>
2025-03-08 23:06:32 +0300

    feat: shared-worker

---
+++ mult.js
@@ -0,0 +1,14 @@
+console.log('Mult.js');
+
+const worker = new SharedWorker("shared.js");
+
+worker.port.onmessage = (event) => {
+    console.log('message from shared.js: ', event.data);
+}
+
+const operation = {
+    data: [1,2,3,4,5],
+    operation: '*'
+}
+
+worker.port.postMessage(operation);
--- worker.js
+++
@@ -1,7 +0,0 @@
-console.log("Hello, World from worker.js");
-
-self.onmessage = (msg) => {
-    console.log('message from main.js: ', msg.data);
-    let list = msg.data || [];
-    self.postMessage(list.reduce((a, b) => a + b, 0));
-}
